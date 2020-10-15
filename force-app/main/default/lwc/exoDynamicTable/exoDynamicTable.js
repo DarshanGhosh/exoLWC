f7d3231285711257995d39c41ab402f724de0ad2
Darshan Ghosh <darshan@MacBook-Darshan.lan>
2020-10-15 09:35:51 +0200

    Exo Dynamic Table first draft

--- force-app/main/default/lwc/exoDynamicTable/exoDynamicTable.js
+++ force-app/main/default/lwc/exoDynamicTable/exoDynamicTable.js
@@ -3,10 +3,17 @@ import getData from "@salesforce/apex/ExoDynamicTable.getData";
 export default class ExoDynamicTable extends LightningElement {
     fields;
     objName;
-    @track data;
-    @track errorMsg
+    @track items = [];
+    @track data = [];
+    @track error;
     @track columns = [];
     @track showTable = false;
+    @track page = 1;
+    @track startingRecord = 1;
+    @track endingRecord = 0;
+    @track pageSize = 5;
+    @track totalRecountCount = 0;
+    @track totalPage = 0;
 
     handleFieldChange(event) {
         this.fields = event.detail.value.split(',');
@@ -18,6 +25,7 @@ export default class ExoDynamicTable extends LightningElement {
 
     generateTable() {
         this.columns = [];
+        this.data = null;
         this.showTable = false;
         this.createColumns();
         this.showTable = true;
@@ -26,11 +34,40 @@ export default class ExoDynamicTable extends LightningElement {
             fields: this.fields
         })
             .then((result) => {
-                this.data = result;
+                this.items = result;
+                this.totalRecountCount = result.length;
+                this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize);
+                this.data = this.items.slice(0, this.pageSize);
+                this.endingRecord = this.pageSize;
+                this.error = undefined;
             })
             .catch((error) => {
-                this.errorMsg = error;
+                this.error = error;
+                this.data = undefined;
             });
+    }
+
+    previousHandler() {
+        if (this.page > 1) {
+            this.page = this.page - 1;
+            this.displayRecordPerPage(this.page);
+        }
+    }
+
+    nextHandler() {
+        if ((this.page < this.totalPage) && this.page !== this.totalPage) {
+            this.page = this.page + 1;
+            this.displayRecordPerPage(this.page);
+        }
+    }
+
+    displayRecordPerPage(page) {
+        this.startingRecord = ((page - 1) * this.pageSize);
+        this.endingRecord = (this.pageSize * page);
+        this.endingRecord = (this.endingRecord > this.totalRecountCount)
+            ? this.totalRecountCount : this.endingRecord;
+        this.data = this.items.slice(this.startingRecord, this.endingRecord);
+        this.startingRecord = this.startingRecord + 1;
     }
 
     createColumns() {
