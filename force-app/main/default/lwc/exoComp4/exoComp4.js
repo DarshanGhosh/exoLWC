ab8546f02ae3b213c58d589dba8698b610f75618
Darshan Ghosh <darshan@macbook-darshan.home>
2020-10-28 13:02:13 +0100

    Update message display

--- force-app/main/default/lwc/exoComp4/exoComp4.js
+++ force-app/main/default/lwc/exoComp4/exoComp4.js
@@ -1,13 +1,31 @@
 import { LightningElement, track } from 'lwc';
 
 export default class ExoComp4 extends LightningElement {
-    @track selectCount = 0;
+    selectCount = 0;
+    @track message;
+
+    connectedCallback() {
+        this.setMessage();
+    }
+
     hanldeChildChange(event) {
         this.selectCount += event.detail;
+        this.setMessage();
     }
 
     handleReset() {
         this.selectCount = 0;
+        this.setMessage();
         this.template.querySelector('c-exo-comp3').reset();
+    }
+
+    setMessage() {
+        if (this.selectCount == 0) {
+            this.message = 'No children selected';
+        } else if (this.selectCount == 1) {
+            this.message = '1 child selected';
+        } else {
+            this.message = this.selectCount + ' children selected';
+        }
     }
 }
