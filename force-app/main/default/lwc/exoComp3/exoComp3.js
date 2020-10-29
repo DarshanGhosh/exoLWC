78b7dbdfdfe67bf6b768f5d3ea3bfdd44c56ea2c
Darshan Ghosh <darshan@MacBook-Darshan.local>
2020-10-29 11:47:48 +0100

    Update exo Grandparent / Parent / Child

--- force-app/main/default/lwc/exoComp3/exoComp3.js
+++ force-app/main/default/lwc/exoComp3/exoComp3.js
@@ -3,19 +3,21 @@ import { LightningElement, track, api } from 'lwc';
 export default class ExoComp3 extends LightningElement {
     isSelectedChild1 = false;
     isSelectedChild2 = false;
-    @track statusChild1 = 'Deselected';
-    @track statusChild2 = 'Deselected';
+    deselected = 'Deselected';
+    selected = 'Selected';
+    @track statusChild1 = this.deselected;
+    @track statusChild2 = this.deselected;
     @api
     reset() {
-        this.statusChild1 = 'Deselected';
+        this.statusChild1 = this.deselected;
         this.template.querySelector('c-exo-comp1').reset();
-        this.statusChild2 = 'Deselected';
+        this.statusChild2 = this.deselected;
         this.template.querySelector('c-exo-comp2').reset();
     }
 
     hanldeOnchild1Change(event) {
         this.isSelectedChild1 = event.detail;
-        this.statusChild1 = this.isSelectedChild1 == true ? 'Selected' : 'Deselected';
+        this.statusChild1 = this.isSelectedChild1 == true ? this.selected : this.deselected;
         var value = this.isSelectedChild1 == true ? 1 : -1;
         const selectedEvent = new CustomEvent("childchange", {
             detail: value
@@ -25,7 +27,7 @@ export default class ExoComp3 extends LightningElement {
     }
     hanldeOnchild2Change(event) {
         this.isSelectedChild2 = event.detail;
-        this.statusChild2 = this.isSelectedChild2 == true ? 'Selected' : 'Deselected';
+        this.statusChild2 = this.isSelectedChild2 == true ? this.selected : this.deselected;
         var value = this.isSelectedChild2 == true ? 1 : -1;
         const selectedEvent = new CustomEvent("childchange", {
             detail: value
